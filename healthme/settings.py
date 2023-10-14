a0b3550dca553de381d884bd648189ea090bc79b
ethicolorr <xex.opy@gmail.com>
2023-10-14 21:12:06 +0300

    fix (settings): add allowed host

--- healthme/settings.py
+++ healthme/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'healthme.up.railway.app']
 
 
 INSTALLED_APPS = [
